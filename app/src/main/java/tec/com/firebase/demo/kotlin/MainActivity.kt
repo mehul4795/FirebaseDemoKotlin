d6536a53aaa28ed4e000d04e6a8fea1a7bf8bd03
mehul4795 <mehul4795@gmail.com>
2020-05-04 23:21:44 +0530

    Added code for the Save and retrieve data from Firebase post.

--- app/src/main/java/tec/com/firebase/demo/kotlin/MainActivity.kt
+++ app/src/main/java/tec/com/firebase/demo/kotlin/MainActivity.kt
@@ -1,12 +1,44 @@
 package tec.com.firebase.demo.kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Database reference pointing to root of database
+        val rootRef: DatabaseReference = FirebaseDatabase.getInstance().reference
+
+        // Database reference pointing to demo node
+        val demoRef: DatabaseReference = rootRef.child("demo")
+
+        btnSubmit.setOnClickListener {
+            val value: String = etValue.text.toString()
+
+            // Push creates a unique id in database
+            demoRef.setValue(value)
+        }
+
+        btnFetch.setOnClickListener {
+            demoRef.addListenerForSingleValueEvent(object : ValueEventListener {
+
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val value = dataSnapshot.getValue(String::class.java)
+                    tvValue.text = value
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Toast.makeText(this@MainActivity, "Error fetching data", Toast.LENGTH_LONG)
+                        .show()
+                }
+            })
+        }
     }
 }
